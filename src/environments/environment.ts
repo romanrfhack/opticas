15ab06c6b0e9adacb3c08e9f549e633e58ebbfc0
romanrfhack <romanrfhack@gmail.com>
2025-11-01 12:30:56 -0600

    api local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   //apiBaseUrl: 'https://opticaapi20250919155555.azurewebsites.net/api'
   //apiBaseUrl: 'http://194.238.26.70/api'
   
-  //apiBaseUrl: '/api',  // Relativo para mismo dominio  
-  apiBaseUrl: 'https://localhost:59744/api'
+  apiBaseUrl: '/api',  // Relativo para mismo dominio  
+  //apiBaseUrl: 'https://localhost:59744/api'
 };
